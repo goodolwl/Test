51f84144279b9090976057dcc2b9076f6331094d
Chris <Chris@CHRIS-PC>
2017-03-14 17:10:55 -0400

    I entered a valid name

--- Notforuse/Notforuse/Program.cs
+++ Notforuse/Notforuse/Program.cs
@@ -11,6 +11,6 @@ namespace Notforuse
         static void Main(string[] args)
         {
         }//I made a thing
-    }//I Chris updated the file
+    }//I Chris updated the file Now with a valid name
     //i am doing this
 }
