9765dd2b5324bd0a2acb891bd2fa046aacb7e1c3
John <techdudeinga@outlook.com>
2017-03-14 17:02:29 -0400

    test

--- Notforuse/Notforuse/Program.cs
+++ Notforuse/Notforuse/Program.cs
@@ -11,8 +11,5 @@ namespace Notforuse
         static void Main(string[] args)
         {
         }//I made a thing
-        //Hello, I am ProgenitorG
-    }//Hello This is from my desktop
-    //Comment on 3/13/2017
-    //Comment after the attempted marge and laptop loggin
-}//hello this if from the laptop
+    }
+}
